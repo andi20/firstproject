35aa4d7d416bc2e7b465f4310fb9f6546d379d0b
Andi <andigurabardhi09@outlook.com>
2019-01-16 03:24:22 -0800

    First Project

--- music/views.py
+++ music/views.py
@@ -4,7 +4,6 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+
 def index(request):
- return HttpResponse("<h1>My music application")
-
-
+    return HttpResponse("<h1>My music application")
